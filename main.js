8e3efbf0b453e7b92dc0c668063913bbdea77519
danielgrebenev2011 <83505072+danielgrebenev2011@users.noreply.github.com>
2021-06-30 10:27:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("my first console project");
+
+var first_name="Daniel";
+var last_name="Grebenev";
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=15;
+var b=15;
+
+var add=a+b;
+console.log(add); 
+
+var subtract=a-b;
+console.log(subtract);
+
+var multiply=a*b;
+console.log(multiply);
+
+var divide=a/b;
+console.log(divide);
